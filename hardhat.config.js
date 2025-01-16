4e17203d20c6f9d2be9196c449d49a7b3c41f7d9
DESKTOP-BEP8RP9\LEKHANG <lehoangkhang12t4@gmail.com>
2025-01-16 15:53:00 +0700

    lần 3 cập nhật địa chỉ ví deloy

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.28",
   networks: {
-    coin: {
+    coinex: {
       url: "https://testnet-rpc.coinex.net",
       accounts: [
         "0x7592c1c2bd952fa6db142f3ec757e398193caacd9e6ac2adf24f409e2f9a4dc0",
